52c756f82bbe58dbded82dc07b62a29b2e9855b6
root <koepkeca@gmail.com>
2016-08-22 00:09:03 -0500

    Unifed, consistant logging

--- tinyRedirect.go
+++ tinyRedirect.go
@@ -1,7 +1,6 @@
 package tinyRedirect
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -44,37 +43,40 @@ func NewEnvData() (e EnvData) {
 
 //Parse extracts a ServerConfig from a EnvData
 func (e EnvData) Parse() (c ServerConfig) {
-	c.ListenString = validateListenString(e.ListenAddr, e.PortNbr)
-	c.RedirType = validateRedirect(e.RedirType)
-	c.RedirTo = validateRedirectTarget(e.RedirTo)
+	c.Logger = log.New(os.Stdout, "tr:", log.Ldate|log.Ltime|log.Lmicroseconds)
+	c.ListenString = c.validateListenString(e.ListenAddr, e.PortNbr)
+	c.RedirType = c.validateRedirect(e.RedirType)
+	c.RedirTo = c.validateRedirectTarget(e.RedirTo)
 	return
 }
 
 // ServerConfig contains server configuration data
 type ServerConfig struct {
-    ListenString string
-    RedirType    int
-    RedirTo      string
+	ListenString string
+	RedirType    int
+	RedirTo      string
+	Logger       *log.Logger
 }
 
-// Run is the main entry point. If you're using daemontools or 
+// Run is the main entry point. If you're using daemontools or
 // docker you want to make sure this method continues running
 // and is not forked or run in a go routine.
 func (c ServerConfig) Run() {
-	http.HandleFunc("/",c.redirector)
+	http.HandleFunc("/", c.redirector)
 	http.HandleFunc("/stat", c.statListener)
-	err := http.ListenAndServe(c.ListenString,nil)
-	log.Print("tinyRedirect starting up..")
+	err := http.ListenAndServe(c.ListenString, nil)
+	c.Logger.Print("tinyRedirect starting up..")
 	if err != nil {
-		log.Fatal("Tinyredirect: ", err)
+		c.Logger.Fatal("Tinyredirect: ", err)
 	}
 	return
 }
 
 // Redirect sends the redirect
 func (c ServerConfig) redirector(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Redirecting to: %s\n", c.RedirTo)
+	c.Logger.Printf("Redirecting to: %s\n", c.RedirTo)
 	http.Redirect(w, r, c.RedirTo, c.RedirType)
+	return
 }
 
 // Poll responds with a HTTP 200, useful to see if the service is up
@@ -84,14 +86,14 @@ func (c ServerConfig) statListener(w http.ResponseWriter, r *http.Request) {
 }
 
 // validateListenString validates and creates the connect string
-func validateListenString(a string, p string) (as string) {
+func (c ServerConfig) validateListenString(a string, p string) (as string) {
 	if a != "" {
 		as = a
 	}
 	as += ":"
 	_, err := strconv.Atoi(p)
 	if err != nil || p == "" {
-		fmt.Printf("Port number missing or invalid, defaulting to %s\n", DEFAULT_INPUT_PORT)
+		c.Logger.Printf("Port number missing or invalid, defaulting to %s\n", DEFAULT_INPUT_PORT)
 		as += DEFAULT_INPUT_PORT
 		return
 	}
@@ -100,15 +102,15 @@ func validateListenString(a string, p string) (as string) {
 }
 
 //validateRedirect validates the redirect type
-func validateRedirect(t string) (rc int) {
+func (c ServerConfig) validateRedirect(t string) (rc int) {
 	rc = DEFAULT_REDIRECT_TYPE
 	code, err := strconv.Atoi(t)
 	if err != nil {
-		fmt.Printf("Invalid redirect, defaulting to %d\n", rc)
+		c.Logger.Printf("Invalid redirect, defaulting to %d\n", rc)
 		return
 	}
 	if code < 300 || code > 309 {
-		fmt.Printf("Redirect code out of bounds, defaulting to %d\n", rc)
+		c.Logger.Printf("Redirect code out of bounds, defaulting to %d\n", rc)
 		return
 	}
 	rc = code
@@ -116,12 +118,10 @@ func validateRedirect(t string) (rc int) {
 }
 
 //validateRedirectTarget validates the redirected target URL
-func validateRedirectTarget(t string) (r string) {
+func (c ServerConfig) validateRedirectTarget(t string) (r string) {
 	r = DEFAULT_REDIRECT_TO
 	if t != "" {
 		r = t
 	}
 	return
 }
-
-
